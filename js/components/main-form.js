74f63ded8642e904d889293675dcdb4c88fb8c15
Juan Lemos <juan900.lemos@gmail.com>
2021-03-18 19:27:27 -0300

    Create the js of submit

---
+++ js/components/main-form.js
@@ -0,0 +1,9 @@
+const submit = window.document.querySelector('.button-link.-submit')
+
+var nome = document.getElementById("name");
+
+submit.addEventListener('click', function() {
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso!');
+    }
+});
