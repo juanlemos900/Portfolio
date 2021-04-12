f414b1b6d9259629e1f98f9cff5ce36d0373a68b
Juan Lemos <juan900.lemos@gmail.com>
2021-04-12 18:37:05 -0300

    Refatore

--- js/components/main-form.js
+++ js/components/main-form.js
@@ -1,9 +1,11 @@
 const submit = window.document.querySelector('.button-link.-submit')
 
-var nome = document.getElementById("name");
+var nome = document.getElementById("name")
 
 submit.addEventListener('click', function() {
-    if (nome.value != "") {
+    if (nome.value != "" ) {
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso!');
+    }else{
+        alert('Insira um nome')
     }
 });
